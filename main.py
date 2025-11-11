1a6afc7c8df6b01191382fb7cd43122b4052adbf
Juaneg0 <juanegomezgomez@gmail.com>
2025-11-11 12:13:10 -0500

    Reconnect_Implementado

--- main.py
+++ main.py
@@ -1,4 +1,29 @@
 import paho.mqtt.client as mqtt
+import time
+
+FIRST_RECONNECT_DELAY = 1
+RECONNECT_RATE = 2
+MAX_RECONNECT_COUNT = 12
+MAX_RECONNECT_DELAY = 60
+
+def on_disconnect(client, userdata, rc):
+    print("Disconnected with result code: {}".format(rc))
+    reconnect_count, reconnect_delay = 0, FIRST_RECONNECT_DELAY
+    while reconnect_count < MAX_RECONNECT_COUNT:
+        print("Reconecting in {} seconds...".format(reconnect_delay))
+        time.sleep(reconnect_delay)
+
+        try:
+            client.reconnect()
+            print("Reconnected successfully!")
+            return
+        except Exception as err:
+            print("Reconnect failed: {}".format(err))
+
+        reconnect_delay *= RECONNECT_RATE
+        reconnect_delay = min(reconnect_delay, MAX_RECONNECT_DELAY)
+        reconnect_count += 1
+    print("Reconnect failed after {} attempts. Exiting...".format(reconnect_count))
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
@@ -10,6 +35,7 @@ def on_message(client, userdata, msg):
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
+client.on_disconnect = on_disconnect
 
 client.connect("localhost", 1883, 60)
 client.loop_forever()
