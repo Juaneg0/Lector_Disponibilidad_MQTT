944e4313cff26a2df6ce5e8897037d1193f90b99
Juaneg0 <juanegomezgomez@gmail.com>
2025-11-12 14:17:28 -0500

    Inserta_Tiempo_Access

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import paho.mqtt.client as mqtt
 import time
+import sys
+from os import getcwd
+import pyodbc
 
 FIRST_RECONNECT_DELAY = 1
 RECONNECT_RATE = 2
@@ -27,10 +30,46 @@ def on_disconnect(client, userdata, rc):
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
-    client.subscribe("#")
+    client.subscribe("Inyectoras/#")
 
 def on_message(client, userdata, msg):
-    print(msg.topic + " " + str(msg.payload))
+    try:
+        topic_parts = msg.topic.split('/')
+
+        # expected topic form: Inyectoras/Inyectora_X/<subtopic>
+        _, machine, subtopic = topic_parts[:3]
+        payload = msg.payload.decode('utf-8', errors='ignore').strip()
+        print(f"Received on {machine}/{subtopic}: {payload}")
+
+        # open a fresh DB connection for this message
+        conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)}; DBQ=C:\Users\FP\Desktop\Bases de Datos\Python\Disponibilidad_Maquinas.accdb;')
+        cursor = conn.cursor()
+
+        # simple dispatch by machine and subtopic
+        if machine == "Inyectora_1":
+            if subtopic == "tiempo_paro":
+                cursor.execute('''
+                INSERT INTO Inyectora_1 (Tiempo_Paro)
+                VALUES (?);
+                ''', (payload,))
+            else:
+                print("Unknown subtopic:", subtopic)
+        elif machine == "Inyectora_2":
+            if subtopic == "tiempo_paro":
+                cursor.execute('''
+                INSERT INTO Inyectora_2 (Tiempo_Paro)
+                VALUES (?);
+                ''', (payload,))
+            else:
+                print("Unknown subtopic:", subtopic)
+        else:
+            print("Unhandled machine:", machine)
+
+        conn.commit()
+        cursor.close()
+        conn.close()
+    except Exception as e:
+        print("on_message error:", e)
 
 client = mqtt.Client()
 client.on_connect = on_connect
@@ -38,4 +77,10 @@ client.on_message = on_message
 client.on_disconnect = on_disconnect
 
 client.connect("localhost", 1883, 60)
+
+""" cursor.execute('SELECT * FROM Inyectora_1;')
+for row in cursor.fetchall():
+    print(row) """
+
+
 client.loop_forever()
